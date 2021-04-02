a72483e58517f7393e0d76f73c6b0786f68edabf
Stephen Walker <Stephen.d.a.walker@gmail.com>
2021-04-01 19:02:43 -0500

    Init repo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  div#app
+    h1 Under Construction...
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
